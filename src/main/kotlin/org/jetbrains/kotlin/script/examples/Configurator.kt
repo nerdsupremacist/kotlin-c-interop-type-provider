690dbfefeff88d40c97f4ea3ed9e5f4f1accc6fc
Mathias Quintero <me@quintero.io>
2020-03-09 14:52:02 +0100

    Renaming annotation

--- src/main/kotlin/org/jetbrains/kotlin/script/examples/Configurator.kt
+++ src/main/kotlin/org/jetbrains/kotlin/script/examples/Configurator.kt
@@ -14,7 +14,7 @@ object Configurator : RefineScriptCompilationConfigurationHandler {
             ?.get(ScriptCollectedData.foundAnnotations)
             ?.mapNotNull { annotation ->
                 when (annotation) {
-                    is Provider -> annotation
+                    is Include -> annotation
                     else -> null
                 }
             }
--- src/main/kotlin/org/jetbrains/kotlin/script/examples/ScriptDefinition.kt
+++ src/main/kotlin/org/jetbrains/kotlin/script/examples/ScriptDefinition.kt
@@ -10,7 +10,7 @@ object ScriptDefinition : ScriptCompilationConfiguration({
 
     defaultImports(
         File::class,
-        Provider::class
+        Include::class
     )
 
     jvm {
@@ -18,7 +18,7 @@ object ScriptDefinition : ScriptCompilationConfiguration({
     }
 
     refineConfiguration {
-        onAnnotations(Provider::class, handler = Configurator)
+        onAnnotations(Include::class, handler = Configurator)
     }
 
     ide {
